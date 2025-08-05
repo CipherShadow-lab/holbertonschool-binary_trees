5833bd307f5ad227d1d1769a543a3f12c28a01fb
cipher <ciphershadow1@gmail.com>
2025-08-05 06:58:56 +0000

    fixed: code and description

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,19 +2,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder: function goes through binary tree using inorder
+ * binary_tree_inorder - function goes through binary tree using inorder
  * @tree: pointer to root node of tree to traverse
  * @func: pointer to a function to call for each node
  *
  * Return: void
  */
 
-void binary_tree_is_inorder(const binary_tree_t *tree, void(*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void(*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_preorder(tree->left, func);
+	binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_inorder(tree->right, func);
 }
