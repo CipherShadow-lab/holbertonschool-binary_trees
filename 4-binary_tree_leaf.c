23bebf498c7fa9b2a982b6b0237134bd91445ff5
CipherShadow <ciphershadow1@gmail.com>
2025-08-05 14:16:00 +1000

    cleaned up temp files

---
+++ 4-binary_tree_leaf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaf: function inserts a leaf node to another node
+ * @parent: pointer of node to check
+ *
+ * Return: 1 if node is leaf otherwise 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node-> == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
