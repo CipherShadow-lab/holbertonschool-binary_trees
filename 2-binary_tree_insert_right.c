e574bf0c06dff7eb25cea1f42b72581acf02ff7b
cipher <ciphershadow1@gmail.com>
2025-08-05 06:37:28 +0000

    fixed: typo in function name

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,7 +9,7 @@
  * Return: pointer to new node or NULL on failure
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *childnode = NULL;
 
