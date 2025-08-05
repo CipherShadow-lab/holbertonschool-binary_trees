dc9632b3b31db3fc5c6e629e422b2e2742234ee2
cipher <ciphershadow1@gmail.com>
2025-08-05 07:17:39 +0000

    fixed: missing parentheses

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,7 +2,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size: function measures size of binary tree
+ * binary_tree_size - function measures size of binary tree
  * @tree: pointer to root node of tree to measure size
  *
  * Return: tree size
@@ -17,10 +17,10 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL || tree->parent == NULL)
 		return (0);
 
-	count_left = binary_tree_size(tree->left);
+	count_left =binary_tree_size(tree->left);
 	count_right = binary_tree_size(tree->right);
 
-	tree_size = (1 + (count_left + count_right);
+	tree_size = (1 + (count_left + count_right));
 
 	return (tree_size);
 }
