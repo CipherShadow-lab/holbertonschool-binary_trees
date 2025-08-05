f99f78f70b231adf6d5a4f689dcec94a12ddf5b5
cipher <ciphershadow1@gmail.com>
2025-08-05 06:56:17 +0000

    fixed: function name

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,14 +2,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder: function goes through binary tree using preorder
+ * binary_tree_preorder - function goes through binary tree using preorder
  * @tree: pointer to root node of tree to traverse
  * @func: pointer to a function to call for each node
  *
  * Return: void
  */
 
-void binary_tree_is_preorder(const binary_tree_t *tree, void(*func)(int))
+void binary_tree_preorder(const binary_tree_t *tree, void(*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
