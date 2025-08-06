b5570a1f6e0f4faca092262553875bc5829e1465
CipherShadow <ciphershadow1@gmail.com>
2025-08-06 14:29:02 +1000

    added task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - function checks if binary tree is perfect
+ * @tree: pointer to root node of tree to check
+ *
+ * Return: 1 if tree is perfect, otherwise 0
+ */
+
+size_t tree_height(const binary_tree_t *tree)
+{
+	size_t left = 0;
+	size_t right = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left = tree_height(tree->left);
+	right = tree_height(tree->right);
+
+	if (left > right)
+		return (1 + left);
+	else
+		return (1 + right);
+}
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+	int left_perfect = 0;
+	int right_perfect = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+
+	if (tree->left == NULL || tree-> right == NULL)
+		return (0);
+
+	left_height = tree_height(tree->left);
+	right_height = tree_height(tree->right);
+
+	left_perfect = binary_tree_is_perfect(tree->left);
+	right_perfect = binary_tree_is_perfect(tree->right);
+
+	if ((left_perfect && right_perfect) && (left_height == right_height))
+		return (1);
+
+	return (0);
+}
