1b8ecd6caf53a135138602fb9769f248dfe14080
CipherShadow <ciphershadow1@gmail.com>
2025-08-06 16:36:33 +1000

    added task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,19 +2,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - function finds a sibling of a ndoe
- * @node: pointer of node to check for sibling
+ * binary_tree_uncle - function finds the uncle of a ndoe
+ * @node: pointer of node to check for uncle
  *
- * Return: pointer to sibling node
+ * Return: pointer to uncle node
  */
 
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
-		return NULL;
+		return (NULL);
 
-	if (node == node->parent->left)
-		return (node->parent->right);
+	if (node->parent->parent == NULL)
+		return (NULL);
+
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
 	else
-		return (node->parent->left);
+		return (node->parent->parent->left);
 }
