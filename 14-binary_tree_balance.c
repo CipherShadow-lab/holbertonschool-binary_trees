e5ad745b48a36d11803c97918fea150621322099
cipher <ciphershadow1@gmail.com>
2025-08-06 04:57:47 +0000

    fix: binary tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,23 +8,27 @@
  * Return: balance factor
  */
 
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height = 0;
-	int right_height = 0;
+	size_t left = 0;
+	size_t right = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-	{
-		left_height = 1 + binary_tree_balance(tree->left);
-	}
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
-	if (tree->right != NULL)
-	{
-		right_height = 1 + binary_tree_balance(tree->right);
-	}
+	if (left > right)
+		return (1 + left);
+	else
+		return (1 + right);
+}
 
-	return (left_height - right_height);
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
